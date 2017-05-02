5dcb8ba1ff98b25c434382763b637cba22c5c64b
milenamaluli <milenamaluli@hotmail.com>
2017-05-02 13:35:19 -0300

    Tentativa do formato da classe

---
+++ classe.py
@@ -0,0 +1,20 @@
+q= int(input('Qual quarto voce esta?'))
+p= int(input('O que voce busca procurar? \n (1)Ocupacao do quarto \n (2)Informacoes'))
+
+
+class Quartos:
+	def __init__(self,o,l,infos):
+		self.o=ocupado
+		self.l=livre
+		self.infos=infos
+
+	def ocupacao(self.o,self.l):
+		if self.o=True:
+			image red
+		if self.l=True:
+			image green
+
+	def informacoes(self, self.infos):
+
+if p==1:
+	if Quartos(p,)
