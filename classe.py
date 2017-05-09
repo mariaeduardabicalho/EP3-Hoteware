51e801d79b62eb301be2da586321a52e2c46291c
milenamaluli <milenamaluli@hotmail.com>
2017-05-09 11:51:41 -0300

    at mi

--- classe.py
+++ classe.py
@@ -1,20 +1,20 @@
 q= int(input('Qual quarto voce esta?'))
 p= int(input('O que voce busca procurar? \n (1)Ocupacao do quarto \n (2)Informacoes'))
 
-
+o=input('O quarto ocupado(o) ou livre(l):')
 class Quartos:
 	def __init__(self,o,l,infos):
-		self.o=ocupado
-		self.l=livre
+		self.o=o
 		self.infos=infos
 
 	def ocupacao(self.o,self.l):
-		if self.o=True:
+		if self.o=o:
 			image red
-		if self.l=True:
+		else:
 			image green
 
 	def informacoes(self, self.infos):
+		print('Horário de almoco: \n13:00-15:00')
 
 if p==1:
 	if Quartos(p,)
