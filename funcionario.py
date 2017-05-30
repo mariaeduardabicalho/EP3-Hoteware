8cf3804951aed2269f403335ed0f346973c7a139
milenamaluli <milenamaluli@hotmail.com>
2017-05-30 14:28:50 -0300

    at 30.05

--- funcionario.py
+++ funcionario.py
@@ -8,28 +8,7 @@ class hotel:
 	
 	def __init__(self):
 		# Janela principal.
-		self.window = tk.Tk()
-		self.window.title("Hotware")
-		self.window.geometry("{0}x{1}+0+0".format(self.window.winfo_screenwidth(),self.window.winfo_screenheight()))
-		self.window.rowconfigure(0, minsize=150, weight=1)
-		self.window.rowconfigure(1, weight=1)
-		self.window.columnconfigure(0, minsize=120, weight=1)
-		self.window.columnconfigure(1, weight=1)
-
-		self.botao=tk.Button(self.window)
-		self.botao.configure(text='Funcionario')
-		self.botao.configure(command=self.wfunci)
-		self.botao.grid()
-
-	def iniciar(self):
-		self.window.mainloop()
-
-	
-	def apertou(self, event):
-		self.postar()
-
-	def wfunci(self):
-		self.tela_user= tk.Toplevel()
+		self.tela_user= tk.Tk()
 		self.tela_user.geometry("{0}x{1}+0+0".format(self.tela_user.winfo_screenwidth(),self.tela_user.winfo_screenheight()))
 		self.tela_user.title("Tela funcionario")
 		self.tela_user.columnconfigure(0, minsize=120, weight=1)
@@ -37,82 +16,143 @@ class hotel:
 		self.tela_user.columnconfigure(2, weight=1)
 		self.tela_user.columnconfigure(3, weight=1)
 
+		self.textoo= tk.StringVar()
+		self.textoo.set('Clique nos quartos azuis para ver notificações')
+		escrever= tk.Label(self.tela_user,textvariable=self.textoo,font=(None,15)).place(x=40,y=600)
+
 		self.botao=tk.Button(self.tela_user)
 		self.botao.configure(text='Quarto 1',background='green')
-		self.botao.configure(command=lambda : self.quarto('Quarto1',0))
+		self.botao.configure(command=lambda : self.quarto('Quarto1',0,0))
 
 		self.botao2=tk.Button(self.tela_user)
 		self.botao2.configure(text='Quarto 2',background='green')
-		self.botao2.configure(command=lambda : self.quarto('Quarto2',1))
+		self.botao2.configure(command=lambda : self.quarto('Quarto2',1,370))
 
 		self.botao3=tk.Button(self.tela_user)
 		self.botao3.configure(text='Quarto 3',background='green')
-		self.botao3.configure(command=lambda : self.quarto('Quarto3',2))
+		self.botao3.configure(command=lambda : self.quarto('Quarto3',2,680))
 
 		self.botao4=tk.Button(self.tela_user)
 		self.botao4.configure(text='Quarto 4',background='green')
-		self.botao4.configure(command=lambda : self.quarto('Quarto4',3))
-		#self.botao4.configure(command= lambda : self.x(" AAA "))
+		self.botao4.configure(command=lambda : self.quarto('Quarto4',3,1000))
 
 		self.botao.grid(row=0,column=0)
 		self.botao2.grid(row=0,column=1)
 		self.botao3.grid(row=0,column=2)
 		self.botao4.grid(row=0,column=3)
+
+		self.mvar=tk.IntVar()
 		
 		self.update_clock()
 
+
+	def iniciar(self):
+		self.tela_user.mainloop()
+
+	
+	def apertou(self, event):
+		self.postar()
+
+
 	def update_clock(self): 
+		g=f.get('/', None)
 		now = time.strftime("%H:%M:%S")
-		self.window.after(1000, self.update_clock)		
-		if (f.get('/users',None)['Quarto1'])=="0" and (f.get('/servico',None)['Quarto1'])=="0" and (f.get('/total',None)['Quarto1'])=="0":
+		self.tela_user.after(1000, self.update_clock)		
+		if (g['users']['Quarto1'])=="0" and (g['servico']['Quarto1'])=="0" and (g['total']['Quarto1'])=="0" and (g['spa']['Quarto1'])=="0" and (g['totalspa']['Quarto1'])=="0" and (g['horariospa']['Quarto1'])=="0":
 			self.botao.configure(background='green')
 		else:
 			self.botao.configure(background='blue')
-		if (f.get('/users',None)['Quarto2'])=="0" and (f.get('/servico',None)['Quarto2'])=="0" and (f.get('/total',None)['Quarto2'])=="0":
+		if (g['users']['Quarto2'])=="0" and (g['servico']['Quarto2'])=="0" and (g['total']['Quarto2'])=="0" and (g['spa']['Quarto2'])=="0" and (g['totalspa']['Quarto2'])=="0" and (g['horariospa']['Quarto2'])=="0":
 			self.botao2.configure(background='green')
 		else:
 			self.botao2.configure(background='blue')
-		if (f.get('/users',None)['Quarto3'])=="0" and (f.get('/servico',None)['Quarto3'])=="0" and (f.get('/total',None)['Quarto3'])=="0":
+		if (g['users']['Quarto3'])=="0" and (g['servico']['Quarto3'])=="0" and (g['total']['Quarto3'])=="0" and (g['spa']['Quarto3'])=="0" and (g['totalspa']['Quarto3'])=="0" and (g['horariospa']['Quarto3'])=="0":
 			self.botao3.configure(background='green')
 		else:
 			self.botao3.configure(background='blue')
-		if (f.get('/users',None)['Quarto4'])=="0" and (f.get('/servico',None)['Quarto4'])=="0" and (f.get('/total',None)['Quarto4'])=="0":
+		if (g['users']['Quarto4'])=="0" and (g['servico']['Quarto4'])=="0" and (g['total']['Quarto4'])=="0" and (g['spa']['Quarto4'])=="0" and (g['totalspa']['Quarto4'])=="0" and (g['horariospa']['Quarto4'])=="0":
 			self.botao4.configure(background='green')
 		else:
 			self.botao4.configure(background='blue')
 
 
-	def quarto(self,q,c):
-		if f.get('/users',None)[q] != '0':
-			self.check=tk.Checkbutton(self.tela_user,text=f.get('/users',None)[q],variable=self.mvar,offvalue=0,command=self.apagarinfou).grid(row=2,column=c)
+	def quarto(self,q,c,p):
+		g=f.get('/', None)
+		le=len(g['servico'][q])
+		le3=len(g['spa'][q])
+		if g['users'][q] != '0':
+
+			self.textoprob= tk.StringVar()
+			self.textoprob.set('Problema reportado: ')
+			escrever= tk.Label(self.tela_user,textvariable=self.textoprob,font=(None,10)).place(x=100+p,y=30)
+			self.check=tk.Checkbutton(self.tela_user,text=g['users'][q],variable=self.mvar,offvalue=0,command=lambda : self.apagarinfou(q)).place(x=110+p,y=60)
 			self.mvar=tk.IntVar()
 			if self.check==1:
 				f.put('/users',q,'0')
 
-		if f.get('/servico',None)[q] != '0':
-			for i in range(len(f.get('/servico',None)[q])):
-				x=f.get('/servico',None)[q][i]
-				self.mvar=tk.IntVar()
-				self.check=tk.Checkbutton(self.tela_user,text=x,variable=self.mvar,command=self.apagarinfos).grid(row=4+i,column=c)
+		if g['servico'][q] != '0':
+			self.textoserv= tk.StringVar()
+			self.textoserv.set('Servico de quarto: ')
+			escrever= tk.Label(self.tela_user,textvariable=self.textoserv,font=(None,10)).place(x=100+p,y=80)
+			for i in range(len(g['servico'][q])):
+				x=g['servico'][q][i]
+				self.mvar3=tk.IntVar()
+				self.check=tk.Checkbutton(self.tela_user,text=x,variable=self.mvar3,command= lambda : self.apagarinfos(q)).place(x=110+p,y=90+(i*20+30))
 				if self.check==1:
 					f.put('/servico',q,x,'0')
 
-
 		self.mvar=tk.IntVar()
-		if f.get('/total',None)[q] != '0':
-			self.check3=tk.Checkbutton(self.tela_user,text='Total: R$' + str(f.get('/total',None)[q])+ ',00',variable=self.mvar,command=self.apagarinfot).grid(row=3,column=c)
+		if g['total'][q]!= '0':
+		
+			self.check3=tk.Checkbutton(self.tela_user,text='Total: R$' + str(g['total'][q])+ ',00',font=(None,10,'bold'),variable=self.mvar,command=lambda : self.apagarinfot(q)).place(x=110+p,y=110+le*30)
 			if self.check3==1:
 				f.put('/total',q,'0')
-	
 
-	def apagarinfou(self):
+
+		if g['spa'][q]!= '0':
+			self.textospa= tk.StringVar()
+			self.textospa.set('Spa: ')
+			escrever= tk.Label(self.tela_user,textvariable=self.textospa,font=(None,10)).place(x=110+p,y=180+le*30)
+			
+			for i in range(len(f.get('/spa',None)[q])):
+				x=g['spa'][q][i]
+				self.mvar=tk.IntVar()
+				self.check=tk.Checkbutton(self.tela_user,text=x,variable=self.mvar,command=lambda : self.apagarinfospa(q)).place(x=110+p,y=220+le*30)
+				if self.check==1:
+					f.put('/spa',q,x,'0')
+
+		self.mvar=tk.IntVar()
+		if g['horariospa'][q]!= '0':
+			self.check3=tk.Checkbutton(self.tela_user,text='Horário no spa:' + str(g['horariospa'][q]),variable=self.mvar,command=lambda : self.apagarinfosh(q)).place(x=110+p,y=200+le*30)
+			if self.check3==1:
+				f.put('/horariospa',q,'0')
+
+		self.mvar=tk.IntVar()
+		if g['totalspa'][q]!= '0':
+			
+			self.check3=tk.Checkbutton(self.tela_user,text='Total: R$' + str(g['totalspa'][q])+ ',00',font=(None,10,'bold'),variable=self.mvar,command=lambda : self.apagarinfots(q)).place(x=110+p,y=250+le*30+le3*20)
+			if self.check3==1:
+				f.put('/totalspa',q,'0')
+
+
+
+	def apagarinfou(self,q):
 		f.put('/users',q,'0')
 
-	def apagarinfos(self):
+	def apagarinfos(self,q):
 		f.put('/servico',q,'0')
 
-	def apagarinfot(self):
+	def apagarinfot(self,q):
 		f.put('/total',q,'0')
+
+	def apagarinfots(self,q):
+		f.put('/totalspa',q,'0')
+
+	def apagarinfospa(self,q):
+		f.put('/spa',q,'0')
+
+	def apagarinfosh(self,q):
+		f.put('/horariospa',q,'0')
 		
 		
 app = hotel()
--- hospede.py
+++ hospede.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 from firebase import firebase
+from PIL import Image, ImageTk
+from tkinter import font
 import time
+from tkinter import Tk, StringVar, ttk
 f = firebase.FirebaseApplication('https://ep3-hotware.firebaseio.com', None)
 
-from tkinter import Tk, StringVar, ttk
 class hotel:
 	
 	def __init__(self):
 		
-		# Janela principal.
+		# Janela de selecao dos quartos
 		self.window = tk.Tk()
 		self.window.title("Hotware")
 		self.window.geometry("500x500")
@@ -18,24 +20,31 @@ class hotel:
 		self.window.columnconfigure(0, minsize=120, weight=1)
 		self.window.columnconfigure(1, weight=1)
 
-		self.texto= tk.StringVar()
+		# Instrucao para selecionar os quartos
+		self.textoq= tk.StringVar()
+		self.textoq.set('Selecione seu quarto')
+		escrever= tk.Label(self.window,textvariable=self.textoq,font=('Comic Sans MS',15)).place(x=80,y=20)
+
+		# Combobox para selecionar os quartos
 		self.combo=ttk.Combobox(self.window)
 		self.combo.place(x=10,y=100,width=200)
-		self.combo['values']=('Qual o numero do seu quarto?','1','2','3','4')
+		self.combo['values']=('---','1','2','3','4')
 		self.combo.current(0)
-		botao=tk.Button(self.window,command=self.ok, text='ok').place(x=80,y=150)
+		botao=tk.Button(self.window,command=self.ok, text='  ok  ', bg='pink').place(x=300,y=100)
 
-
-		self.bo=tk.Button(self.window)
-		self.bo.grid()
-		self.bo.configure(text='hospede')
-		self.bo.configure(command=self.whospede)
+		# Lista para armazenar os valores das comidas e as comidas 
 		self.A=[]
 		self.B=[]
+		self.S=[]
+		self.V=[]
+		self.H=[]
+
+		#Dicionario que armazena o valor de cada alimento
 		self.entradas={'salame-R$2,00':2, 'batata-R$5,00':5}
 		self.pratop={'Penne a Bolognesa-R$35,00':35,'Filet ao molho de mostarda-R$40,00':40}
 		self.sobremesa={'Torta de Morango-R$15,00':15,'Petit Gateau-R$20,00':20}
 		self.bebida={'Refrigerante-R$5,00':5,'Agua-R$3,00':3,'Suco Natural-R$8,00':8}
+		self.spa1={'Unha da mão-R$15,00':15,'Unha do pé-R$20,00':20,'Massagem-R$45,00 ':45}
 
 	def ok(self):
 		if self.combo.get()=='1':
@@ -46,8 +55,20 @@ class hotel:
 			self.q= 'Quarto3'
 		if self.combo.get()=='4':
 			self.q= 'Quarto4'
+
+
+		self.tela_user= tk.Toplevel()
+		self.tela_user.geometry("800x800")
+		self.tela_user.title("Tela hospede")
+
+		self.menubar = tk.Menu(self.tela_user)
+		self.menubar.add_command(label="Informacoes", command=self.inform)
+		self.menubar.add_command(label="Reportar Problemas", command=self.reportarproblemas)
+		self.menubar.add_command(label="Servico de Quarto", command=self.servicodequarto)
+		self.menubar.add_command(label="Spa", command=self.spa)
+		self.tela_user.config(menu=self.menubar) # Como permutar entre menus?
+
 	
-		
 	def iniciar(self):
 		self.window.mainloop()
 
@@ -55,20 +76,60 @@ class hotel:
 	def apertou(self, event):
 		self.postar(p)
 
-	def whospede(self):
-		self.tela_user= tk.Toplevel()
-		self.tela_user.geometry("800x800")
-		self.tela_user.title("Tela hospede")
 
-		self.menubar = tk.Menu(self.tela_user)
-		self.menubar.add_command(label="Inform", command=self.inform)
-		self.menubar.add_command(label="Reportar Problemas", command=self.reportarproblemas)
-		self.menubar.add_command(label="Servico de Quarto", command=self.servicodequarto)
-		self.tela_user.config(menu=self.menubar) # Como permutar entre menus?
+	def spa(self):
+		self.textoo= tk.StringVar()
+		self.textoo.set('Escolha um serviço do SPA')
+		escrever= tk.Label(self.tela_user,textvariable=self.textoo,font=('Comic Sans MS',15)).place(x=40,y=20)
+
+		self.texto6= tk.StringVar()
+		self.texto6.set('Serviços:')
+		self.texto8= tk.StringVar()
+		self.texto8.set('Total:')
+		self.combo6=ttk.Combobox(self.tela_user)
+		self.combo6.place(x=10,y=100,width=160)
+		self.combo6['values']=('Escolha um serviço','Unha da mão-R$15,00','Unha do pé-R$20,00','Massagem-R$45,00 ')
+		self.combo6.current(0)
+		self.combo8=ttk.Combobox(self.tela_user)
+		self.combo8.place(x=300,y=100,width=200)
+		self.combo8['values']=('Escolha um horario de chegada','9:00','10:00','11:00','12:00','14:00','15:00')
+		self.combo8.current(0)
+		botao6=tk.Button(self.tela_user,command=self.pegars, text='Adicionar').place(x=80,y=150)
+		botao8=tk.Button(self.tela_user,command=self.finalizars, text='Finalizar').place(x=80,y=180)
+		etiqueta6= tk.Label(self.tela_user,textvariable=self.texto6).place(x=40,y=210)
+		etiqueta8= tk.Label(self.tela_user,textvariable=self.texto8).place(x=40,y=240)
+		#self.ph=tk.PhotoImage(file="massagemmulher.png")
+		# fazer:  image= tk.Image.open()
+
+
+	def pegars(self):
+		if self.combo6.get()!='Escolha um serviço':
+			self.S.append(self.combo6.get())
+			self.V.append(self.spa1[self.combo6.get()])
+		if self.combo8.get()!='Escolha um horario':
+			self.H.append(self.combo8.get())
+		self.texto6.set('Serviços: ' + str(self.S))
+		
+
+	def finalizars(self):
+		self.texto8.set('Total: R$'+ str(sum(self.V)) +',00')
+		f.get('/spa',None)
+		f.get('/totalspa',None)
+		f.get('/horariospa',None)
+		teste = self.q
+		f.put('/spa',teste,self.S)
+		f.put('/totalspa',teste,sum(self.V))
+		f.put('/horariospa',teste,self.H)
+		self.texto3= tk.StringVar()
+		self.texto3.set('Seu horário foi reservado!')
+		feedback= tk.Label(self.tela_user,textvariable=self.texto3).place(x=100,y=300)
 
 
 
 	def servicodequarto(self):
+		self.textoo= tk.StringVar()
+		self.textoo.set('Escolha seu pedido. Se quiser adicionar mais de um do mesmo topico, tenha certeza de que os outros não estao selecionados.')
+		escrever= tk.Label(self.tela_user,textvariable=self.textoo,font=(None,10)).place(x=40,y=20)
 			
 		self.texto= tk.StringVar()
 		self.texto2=tk.StringVar()
@@ -136,16 +197,12 @@ class hotel:
 		caixa_texto.configure(textvariable=self.conteudo_caixa_texto)
 		caixa_texto.grid(row=1, column=0, padx=20, sticky="ew")
 
-		caixa_texto.bind("<Return>", self.apertou_enter)
-
 		        
 		botão = tk.Button(self.tela_user)
 		botão.configure(text="Postar")
 		botão.configure(command=self.postarprob)
 		botão.grid(row=1, column=1)
 
-	def apertou_enter(self, event):
-		self.postar()
 
 	
 	def postarprob(self):
@@ -162,14 +219,23 @@ class hotel:
 
 	def inform(self):
 
-		Lb1 = tk.Listbox(self.tela_user,width=200)
+		Lb1 = tk.Listbox(self.tela_user,width=30,height=4)
+		Lb1.place(x=0, y=0)
 		now = time.strftime("%H:%M:%S")
-		print(now)
 
 		Lb1.insert(0, "Café da manhã:  07:00h as 10:30h")
 		Lb1.insert(1, "Almoco:  12:00h as 15:00h")
 		Lb1.insert(2, "Jantar:  18:00h as 22:00h")
 		Lb1.insert(3, "Piscina: 08:00h as 22:00h")
+
+		Lb2 = tk.Listbox(self.tela_user,width=30,height=2)
+		Lb2.place(x=100, y=0)
+		Lb2.insert(0, "Fechado")
+		Lb2.itemconfig(0, bg='red',)
+		Lb2.insert(1, "Aberto")
+		Lb2.itemconfig(1, bg='green')
+
+
 
 		#mudança de cor da listbox de acordo com horario de funcionamento
 		if now >= time.strftime("07:00:00") and (now<time.strftime("10:30:00")) :
@@ -195,3 +261,10 @@ class hotel:
 
 app = hotel()
 app.iniciar()
+
+
+
+#trocar lugar spa cmbobox
+#arrumar pedidos
+#clicar no ok + de 3 vzs pos pedido
+#ajustar feeedback
